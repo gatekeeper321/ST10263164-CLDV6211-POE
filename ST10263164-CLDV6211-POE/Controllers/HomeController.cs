01f3a7c5b407e0f0eedda5cf69a97cc33fa105d6
william <william@DESKTOP-AQ9IQ7E>
2024-04-02 16:02:01 +0200

    Add project files.

---
+++ ST10263164-CLDV6211-POE/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using ST10263164_CLDV6211_POE.Models;
+using System.Diagnostics;
+
+namespace ST10263164_CLDV6211_POE.Controllers
+{
+    //William McPetrie ST10263164 CLDV POE
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MyWork()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
